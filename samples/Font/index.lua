d478fbb46182ab4693a912b7d45dfa077d098f3a
Christopher <christopher.roelofs@gmail.com>
2025-06-23 17:58:38 -0400

    add otf font support

--- samples/Font/index.lua
+++ samples/Font/index.lua
@@ -1,5 +1,6 @@
 -- Loading a TTF font
 local fnt = Font.load("main.ttf")
+local otf = Font.load("LibertinusSerif-Semibold.otf")
 defaultFont = Font.load()
 
 -- Main loop
@@ -12,9 +13,10 @@ while true do
 	Screen.clear()
 	
 	-- Drawing something on screen
-	Font.print(fnt, 5, 45, "This is a local font", Color.new(255, 255, 255))
-	Font.print(defaultFont, 5, 100, "This is the default font", Color.new(255, 255, 255))
-	Font.print(fnt, 5, 150, "Press ESC to exit.", Color.new(255, 255, 255))
+	Font.print(fnt, 5, 45, "This is a local ttf font", Color.new(255, 255, 255))
+	Font.print(otf, 5, 100, "This is a local otf font", Color.new(255, 255, 255))
+	Font.print(defaultFont, 5, 150, "This is the default font", Color.new(255, 255, 255))
+	Font.print(fnt, 5, 200, "Press ESC to exit.", Color.new(255, 255, 255))
 	
 	-- Terminating GPU rendering
 	Graphics.termBlend()
